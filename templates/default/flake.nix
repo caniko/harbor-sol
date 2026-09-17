51fd8d64bf76a3201f669fa038452a6f225f793e
Can H. Tartanoglu <gpg@rotas.mozmail.com>
2026-09-17 09:14:00 +0200

    chore: migrate harbor inputs to github and update flake locks

--- templates/default/flake.nix
+++ templates/default/flake.nix
@@ -4,11 +4,13 @@
   inputs = {
     harbor-sol.url = "github:caniko/harbor-sol";
     nixpkgs.follows = "harbor-sol/nixpkgs";
+    treefmt-nix.follows = "harbor-sol/treefmt-nix";
   };
 
   outputs = {
     harbor-sol,
     nixpkgs,
+    treefmt-nix,
     ...
   }: let
     systems = ["x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin"];
@@ -22,6 +24,14 @@
       };
     });
 
-    formatter = nixpkgs.lib.genAttrs systems (system: (import nixpkgs {inherit system;}).alejandra);
+    formatter = nixpkgs.lib.genAttrs systems (system:
+      (treefmt-nix.lib.evalModule nixpkgs.legacyPackages.${system} {
+        imports = [
+          harbor-sol.inputs.harbor-meta.treefmtModules.nix
+          harbor-sol.inputs.harbor-meta.treefmtModules.toml
+          harbor-sol.inputs.harbor-rs.treefmtModules.rust
+        ];
+        projectRootFile = "flake.nix";
+      }).config.build.wrapper);
   };
 }
